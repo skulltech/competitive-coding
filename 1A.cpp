06deaa8753faa667ad955a7f77fa8e50d1be1f86
SkullTech <sumit.ghosh32@gmail.com>
2016-09-11 15:22:37 +0530

    First commit for problem 1A

---
+++ 1A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <sstream>
+using namespace std;
+
+
+int algorithm(int length, int breadth, int tile_size)
+{
+	int rows, cols;
+
+	rows = ceil((float)breadth/(float)tile_size);
+	cols = ceil((float)length/(float)tile_size);
+
+	return rows*cols;
+}
+
+
+int main()
+{
+	int n, m, a;
+	int cols, rows;
+
+	string line;
+	while (getline(cin, line))
+	{
+		istringstream iss(line);
+		int m, n, a;
+
+		if (iss >> m >> n >> a)
+			cout << algorithm(m, n, a) << endl;
+			break;
+	}
+
+	return 0;
+}
