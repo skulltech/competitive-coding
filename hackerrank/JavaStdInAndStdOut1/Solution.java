bf3bdcdba04e0563d4ec826803acd8f310a2e989
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-13 20:01:51 +0530

    Basic Idea setup and some preliminary challenges

---
+++ hackerrank/JavaStdInAndStdOut1/Solution.java
@@ -0,0 +1,15 @@
+package hackerrank.JavaStdInAndStdOut1;
+
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scanner.nextInt());
+        }
+    }
+}
